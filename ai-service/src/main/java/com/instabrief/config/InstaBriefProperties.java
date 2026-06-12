8f7ccbd0628a196a5ce761b1eceb238d428f04e6
Chanithi Vinodya <bandarabvcv@gmail.com>
2026-06-12 11:01:20 +0530

    Add nlpServiceUrl to InstaBriefProperties

--- ai-service/src/main/java/com/instabrief/config/InstaBriefProperties.java
+++ ai-service/src/main/java/com/instabrief/config/InstaBriefProperties.java
@@ -21,6 +21,7 @@ public class InstaBriefProperties {
     private long trendingFixedDelayMs = 900_000;
     private int maxKeywords = 8;
     private int summaryMaxLength = 1024;
+    private String nlpServiceUrl = "http://nlp-service:3005";
 
     @PostConstruct
     public void validate() {
@@ -36,30 +37,115 @@ public class InstaBriefProperties {
         }
     }
 
-    public String getNewsApiKey() { return newsApiKey; }
-    public void setNewsApiKey(String newsApiKey) { this.newsApiKey = newsApiKey; }
-    public String getNewsApiUrl() { return newsApiUrl; }
-    public void setNewsApiUrl(String newsApiUrl) { this.newsApiUrl = newsApiUrl; }
-    public String getNewsApiCountry() { return newsApiCountry; }
-    public void setNewsApiCountry(String newsApiCountry) { this.newsApiCountry = newsApiCountry; }
-    public int getNewsApiPageSize() { return newsApiPageSize; }
-    public void setNewsApiPageSize(int newsApiPageSize) { this.newsApiPageSize = newsApiPageSize; }
-    public String getRssFeedUrls() { return rssFeedUrls; }
-    public void setRssFeedUrls(String rssFeedUrls) { this.rssFeedUrls = rssFeedUrls; }
-    public String getHfApiToken() { return hfApiToken; }
-    public void setHfApiToken(String hfApiToken) { this.hfApiToken = hfApiToken; }
-    public String getHfSummaryModel() { return hfSummaryModel; }
-    public void setHfSummaryModel(String hfSummaryModel) { this.hfSummaryModel = hfSummaryModel; }
-    public String getHfApiUrl() { return hfApiUrl; }
-    public void setHfApiUrl(String hfApiUrl) { this.hfApiUrl = hfApiUrl; }
-    public boolean isIngestionEnabled() { return ingestionEnabled; }
-    public void setIngestionEnabled(boolean ingestionEnabled) { this.ingestionEnabled = ingestionEnabled; }
-    public long getIngestionFixedDelayMs() { return ingestionFixedDelayMs; }
-    public void setIngestionFixedDelayMs(long ingestionFixedDelayMs) { this.ingestionFixedDelayMs = ingestionFixedDelayMs; }
-    public long getTrendingFixedDelayMs() { return trendingFixedDelayMs; }
-    public void setTrendingFixedDelayMs(long trendingFixedDelayMs) { this.trendingFixedDelayMs = trendingFixedDelayMs; }
-    public int getMaxKeywords() { return maxKeywords; }
-    public void setMaxKeywords(int maxKeywords) { this.maxKeywords = maxKeywords; }
-    public int getSummaryMaxLength() { return summaryMaxLength; }
-    public void setSummaryMaxLength(int summaryMaxLength) { this.summaryMaxLength = summaryMaxLength; }
+    public String getNewsApiKey() {
+        return newsApiKey;
+    }
+
+    public void setNewsApiKey(String newsApiKey) {
+        this.newsApiKey = newsApiKey;
+    }
+
+    public String getNewsApiUrl() {
+        return newsApiUrl;
+    }
+
+    public void setNewsApiUrl(String newsApiUrl) {
+        this.newsApiUrl = newsApiUrl;
+    }
+
+    public String getNewsApiCountry() {
+        return newsApiCountry;
+    }
+
+    public void setNewsApiCountry(String newsApiCountry) {
+        this.newsApiCountry = newsApiCountry;
+    }
+
+    public int getNewsApiPageSize() {
+        return newsApiPageSize;
+    }
+
+    public void setNewsApiPageSize(int newsApiPageSize) {
+        this.newsApiPageSize = newsApiPageSize;
+    }
+
+    public String getRssFeedUrls() {
+        return rssFeedUrls;
+    }
+
+    public void setRssFeedUrls(String rssFeedUrls) {
+        this.rssFeedUrls = rssFeedUrls;
+    }
+
+    public String getHfApiToken() {
+        return hfApiToken;
+    }
+
+    public void setHfApiToken(String hfApiToken) {
+        this.hfApiToken = hfApiToken;
+    }
+
+    public String getHfSummaryModel() {
+        return hfSummaryModel;
+    }
+
+    public void setHfSummaryModel(String hfSummaryModel) {
+        this.hfSummaryModel = hfSummaryModel;
+    }
+
+    public String getHfApiUrl() {
+        return hfApiUrl;
+    }
+
+    public void setHfApiUrl(String hfApiUrl) {
+        this.hfApiUrl = hfApiUrl;
+    }
+
+    public boolean isIngestionEnabled() {
+        return ingestionEnabled;
+    }
+
+    public void setIngestionEnabled(boolean ingestionEnabled) {
+        this.ingestionEnabled = ingestionEnabled;
+    }
+
+    public long getIngestionFixedDelayMs() {
+        return ingestionFixedDelayMs;
+    }
+
+    public void setIngestionFixedDelayMs(long ingestionFixedDelayMs) {
+        this.ingestionFixedDelayMs = ingestionFixedDelayMs;
+    }
+
+    public long getTrendingFixedDelayMs() {
+        return trendingFixedDelayMs;
+    }
+
+    public void setTrendingFixedDelayMs(long trendingFixedDelayMs) {
+        this.trendingFixedDelayMs = trendingFixedDelayMs;
+    }
+
+    public int getMaxKeywords() {
+        return maxKeywords;
+    }
+
+    public void setMaxKeywords(int maxKeywords) {
+        this.maxKeywords = maxKeywords;
+    }
+
+    public int getSummaryMaxLength() {
+        return summaryMaxLength;
+    }
+
+    public void setSummaryMaxLength(int summaryMaxLength) {
+        this.summaryMaxLength = summaryMaxLength;
+    }
+
+    public String getNlpServiceUrl() {
+        return nlpServiceUrl;
+    }
+
+    public void setNlpServiceUrl(String nlpServiceUrl) {
+        this.nlpServiceUrl = nlpServiceUrl;
+    }
 }
